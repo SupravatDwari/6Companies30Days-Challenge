e12f1ee2b40480bbe83845ef0ee77db2037aaa96
Supravat Dwari <86393808+SupravatDwari@users.noreply.github.com>
2023-01-01 20:59:23 +0530

    Day 01

---
+++ Microsoft/Bulls_And_Cows.java
@@ -0,0 +1,26 @@
+/* Leetcode Link : https://leetcode.com/problems/bulls-and-cows/description/ */
+
+class Solution {
+    public String getHint(String secret, String guess) {
+        int n = secret.length();
+        int s[] = new int[10];
+        int g[] = new int[10];
+        int bulls = 0, cows = 0;
+        for(int i=0; i<n; i++){
+            if(guess.charAt(i)==secret.charAt(i))
+                bulls++;
+            else{
+                s[(int)secret.charAt(i)-48]++;
+                g[(int)guess.charAt(i)-48]++;   
+            }
+        }
+        for(int i=0; i<10; i++)
+            cows += Math.min(s[i],g[i]);
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(bulls).append("A").append(cows).append("B");
+        return sb.toString();
+    }
+}
+
+/* Submission Link : https://leetcode.com/problems/bulls-and-cows/submissions/869064080/ */
