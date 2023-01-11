e3b03e8f7fd00af5ebe6dc086830f0baa853e108
Supravat Dwari <86393808+SupravatDwari@users.noreply.github.com>
2023-01-11 15:00:35 +0530

    Day 08

--- Goldman Sachs/Count_Good_Triplets_in_an_Array.java
+++ Goldman Sachs/Count_Good_Triplets_in_an_Array.java
@@ -1,26 +1,69 @@
 /* Leetcode Link : https://leetcode.com/problems/count-good-triplets-in-an-array/description/ */
 
-class Solution {
-    public String getHint(String secret, String guess) {
-        int n = secret.length();
-        int s[] = new int[10];
-        int g[] = new int[10];
-        int bulls = 0, cows = 0;
-        for(int i=0; i<n; i++){
-            if(guess.charAt(i)==secret.charAt(i))
-                bulls++;
-            else{
-                s[(int)secret.charAt(i)-48]++;
-                g[(int)guess.charAt(i)-48]++;   
-            }
-        }
-        for(int i=0; i<10; i++)
-            cows += Math.min(s[i],g[i]);
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append(bulls).append("A").append(cows).append("B");
-        return sb.toString();
+class FenwickTree {
+  public FenwickTree(int n) {
+    sums = new int[n + 1];
+  }
+
+  public void update(int i, int delta) {
+    while (i < sums.length) {
+      sums[i] += delta;
+      i += lowbit(i);
     }
+  }
+
+  public int get(int i) {
+    int sum = 0;
+    while (i > 0) {
+      sum += sums[i];
+      i -= lowbit(i);
+    }
+    return sum;
+  }
+
+  private int[] sums;
+
+  private static int lowbit(int i) {
+    return i & -i;
+  }
+}
+
+class Solution {
+  public long goodTriplets(int[] nums1, int[] nums2) {
+    final int n = nums1.length;
+    long ans = 0;
+    Map<Integer, Integer> numToIndex = new HashMap<>();
+    int[] A = new int[n];
+    // leftSmaller[i] := # of A[j] < A[i], where 0 <= j < i
+    int[] leftSmaller = new int[n];
+    // rightLarger[i] := # of A[j] > A[i], where i < j < n
+    int[] rightLarger = new int[n];
+    FenwickTree tree1 = new FenwickTree(n); // To calculate leftSmaller
+    FenwickTree tree2 = new FenwickTree(n); // To calculate rightLarger
+
+    for (int i = 0; i < n; ++i)
+      numToIndex.put(nums1[i], i);
+
+    // Remap each num in nums2 to the according index in nums1 as A
+    // Then, rephrase the problem as finding increasing tripets in A
+    for (int i = 0; i < n; ++i)
+      A[i] = numToIndex.get(nums2[i]);
+
+    for (int i = 0; i < n; ++i) {
+      leftSmaller[i] = tree1.get(A[i]);
+      tree1.update(A[i] + 1, 1);
+    }
+
+    for (int i = n - 1; i >= 0; --i) {
+      rightLarger[i] = tree2.get(n) - tree2.get(A[i]);
+      tree2.update(A[i] + 1, 1);
+    }
+
+    for (int i = 0; i < n; ++i)
+      ans += (long) leftSmaller[i] * rightLarger[i];
+
+    return ans;
+  }
 }
 
 /* Submission Link : https://leetcode.com/problems/count-good-triplets-in-an-array/submissions/875859774/ */
